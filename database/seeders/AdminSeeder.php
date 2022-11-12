7a0913286cfa457b6ab3ef8dd91cdb20926dd630
Hanani Tallyson <tallysonhanani@gmail.com>
2022-11-12 20:52:14 -0300

    Update AdminSeeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -17,14 +17,14 @@ class AdminSeeder extends Seeder
     {
         User::create([
             'name' => 'admin',
-            'email' => 'admin@gmail.com',
+            'email' => 'admin@admin',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ])->assignRole('admin');
 
         User::create([
             'name' => 'teste',
-            'email' => 'teste@gmail.com',
+            'email' => 'teste@teste',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ])->assignRole('user');
